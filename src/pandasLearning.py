2c00c16b713581e31b0ac695b5bdc1e9e65a52c0
bibongbong@gmailcom <bibongbong@gmail.com>
2018-10-26 23:57:04 +0800

    [add] olympic.cvs; [modificaiton] pandasLearning.py

--- src/pandasLearning.py
+++ src/pandasLearning.py
@@ -108,19 +108,76 @@ print(df)
 
 
 ##对CSV文件的操作
-df = pd.read_csv('..\cfg\mysameple.csv')
-print(df)
-#             0                   1      ...                   8              9
-#0  SRM_SaaS_ES  MXPERUSERInterface      ...                   d              d
-#1      LOGINID            PERSONID      ...        ADDRESSLINE3  BILLTOADDRESS
-#2     manoel11            manoel11      ...                   e  BILLTOADDRES
-#[3 rows x 10 columns]
+# 解决列过多时，省略显示的问题
+pd.set_option('display.max_columns', None)
+
+df = pd.read_csv('..\cfg\olympic.csv', encoding='ANSI')
+#print(df)
+#                    0            1     2     3     4      5             6     7     8     9     10     11    12    13    14              15
+# 0               NaN  Summer Games  01 !  02 !  03 !  Total  Winter Games  01 !  02 !  03 !  Total  Games  01 !  02 !  03 !  Combined Total
+# 1  Afghanistan(AFG)            14     0     0     2      2             0     0     0     0      0     14     0     0     2               2
+# 2      Algeria(ALG)            13     5     4     8     17             3     0     0     0      0     16     5     4     8              17
+# 3    Argentina(ARG)            24    21    25    28     74            19     0     0     0      0     43    21    25    28              74
+# 4      Armenia(ARM)             6     2     6     6     14             7     0     0     0      0     13     2     6     6              14
+
 
 # 我们可以使用index_col来指示哪一列是索引，也可以使用header参数来指示文件中的哪一行可以用来作为标签
 # 重新导入输入，设置index_col=0，这是第一列。使用skiprows来设置列的标签，忽略第一行，从第二行开始读取
-df = pd.read_csv('..\cfg\mysameple.csv', index_col=0, skiprows=1)
-print(df)
-#            MXPERUSERInterface      ...                  d.5
-#SRM_SaaS_ES                         ...
-#LOGINID               PERSONID      ...        BILLTOADDRESS
-#manoel11              manoel11      ...        BILLTOADDRESS
+df = pd.read_csv('..\cfg\olympic.csv', encoding='ANSI', index_col=0, skiprows=1)
+#print(df.head()) # 输出如下
+#                NaN  Summer Games  01 !  02 !  03 !  Total  Winter Games  01 !.1  02 !.1  03 !.1  Total  Games  01 !.2  02 !.2  03 !.2  Combined Total
+#   Afghanistan(AFG)            14     0     0     2      2             0       0       0       0      0     14       0       0       2               2
+#       Algeria(ALG)            13     5     4     8     17             3       0       0       0      0     16       5       4       8              17
+#     Argentina(ARG)            24    21    25    28     74            19       0       0       0      0     43      21      25      28              74
+#      Armenia(ARM)              6     2     6     6     14             7       0       0       0      0     13       2       6       6              14
+
+
+# 当column中有相同的名字时，会在重复的名字后加‘.1’,后缀，如上显示的 01!.01  01!.02。 我们可以通过Pandas来修改
+# Pandas把所有的column的名字保存在 .columns属性中
+print(df.columns)
+# Index(['Summer Games', '01 !', '02 !', '03 !', 'Total', 'Winter Games',
+#        '01 !.1', '02 !.1', '03 !.1', 'Total.1', 'Games', '01 !.2', '02 !.2',
+#        '03 !.2', 'Combined Total'],
+#      dtype='object')
+
+# 我们可以通过rename来对column改名
+# inplace设为True，立即更新df，而不是返回副本
+for col in df.columns:
+    if col[:2] == '01':
+        df.rename(columns={col:'Gold'+col[4:]}, inplace=True)
+    elif col[:2] == '02':
+        df.rename(columns={col:'Silver'+col[4:]}, inplace=True)
+    elif col[:2] == '03':
+        df.rename(columns={col:'Bronze'+col[4:]}, inplace=True)
+#print(df.head()) # 输出如下
+#                         Summer Games Gold Silver! Bronze! Total  Winter Games  \
+# Afghanistan(AFG)                  14    0       0       2     2             0
+# Algeria(ALG)                      13    5       4       8    17             3
+# Argentina(ARG)                    24   21      25      28    74            19
+# Armenia(ARM)                       6    2       6       6    14             7
+# Australasia(ANZ) [ANZ]             2    3       4       5    12             0
+
+#                        Gold.1  Silver.1  Bronze.1 Total.1  Games Gold.2  \
+# Afghanistan(AFG)            0         0         0       0     14      0
+# Algeria(ALG)                0         0         0       0     16      5
+# Argentina(ARG)              0         0         0       0     43     21
+# Armenia(ARM)                0         0         0       0     13      2
+# Australasia(ANZ) [ANZ]      0         0         0       0      2      3
+
+#                         Silver.2  Bronze.2 Combined Total
+# Afghanistan(AFG)               0         2              2
+# Algeria(ALG)                   4         8             17
+# Argentina(ARG)                25        28             74
+# Armenia(ARM)                   6         6             14
+# Australasia(ANZ) [ANZ]         4         5             12
+
+
+##################################################
+#           Querying a DataFrame                 #
+##################################################
+# 布尔屏蔽 (boolean masking) 是numpy快速查询的核心
+# 布尔屏蔽 可以是一维的series，也可以是二维的dataFrame，里面的值都是True或者False，用来覆盖在我们查询的数据解构上
+# 任何对应True的元素都会进入到我们的结果里， False的则不会
+# 这个强大的功能，应用极广，比如图像
+df = pd.read_csv('..\cfg\olympic.csv', encoding='ANSI')
+#print(df.head())
